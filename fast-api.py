308eedb3e89c605a25c2e156781b6ef787cd1768
potexxi <maximilian.schreiter@outlook.at>
2026-03-26 09:10:20 +0100

    commit

---
+++ fast-api.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/water")
+def get_wasserstellen():
+    return [
+        {"lat": 48.2082, "lon": 16.3738},
+        {"lat": 48.2100, "lon": 16.3700}
+    ]
